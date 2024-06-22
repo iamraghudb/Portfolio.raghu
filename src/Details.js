a15c5643bdf6af832775ed245bf743fc0f19c1af
Innowave <innowave@Innowaves-MacBook-Pro.local>
2024-06-22 14:25:47 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,191 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+import raghu from "./assets/raghu.png";
+
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import reactNative from "./assets/techstack/reactNative.svg";
+import typescript from "./assets/techstack/tyepscript.png";
+import mui from "./assets/techstack/mui.png";
+
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+  raghu: raghu,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Raghavendra",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Experienced Frontend Developer proficient in HTML, CSS,
+JavaScript, React, Redux, React Native, Tailwind CSS, and
+Material-UI. With a background in diverse projects including IoT,
+healthcare mobile apps, and CRM platforms for education,I bring
+a proven track record in delivering high-quality, user-centric
+solutions. Passionate about leveraging technology to drive
+innovation and solve complex problems in various domains`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/raghavendra-db-580499166/",
+  github: "https://github.com/iamraghudb",
+  twitter: "https://x.com/iamraghudb",
+  instagram: "https://www.instagram.com/iamraghudb",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Developer",
+    Company: `Akademe Edify Technologies Private Limited`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Aug 2023 - Present",
+  },
+  {
+    Position: "Frontend Developer",
+    Company: `Innowave India`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Aug 2021 - Aug 2023",
+  },
+  {
+    Position: "Frontend Developer",
+    Company: `PointCross: Life Science Software Solutions`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Feb 2021 - Augu 2021",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Computer Science And Engg",
+    Company: "Jain Institute Of Technology",
+    Location: "Davangere",
+    Duration: "2014 - 2018",
+  },
+  {
+    Position: "Computer Science And Engg",
+    Company: `DRR Govt Polytechnic`,
+    Location: "Davangere",
+    Duration: "2011 - 2014",
+  },
+  {
+    Position: "S S L C",
+    Company: `Sri Someshwara Vidyalaya`,
+    Location: "Davangere",
+    Duration: "2010 - 2011",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  reactNative: reactNative,
+  typescript: typescript,
+  mui: mui,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "raghavendradb6@gmail.com",
+  phone: "+91 8892912034",
+};
